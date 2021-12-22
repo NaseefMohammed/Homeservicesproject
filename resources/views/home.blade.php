511f8a1d1f60255880be1badb0a1272d8102e4ab
naseef <naseefm918@gmail.com>
2021-12-22 03:13:06 -0800

    first commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,14 +5,44 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+<link rel="stylesheet" href="{{url('css/style.css')}}" type="text/css">
 </head>
 <body>
-    <h1>home page</h1>
-<p> new page</p>
-<p>new change</p>
+    <div class="top-container">
+        <img class="top-cloud"src="{{url('media/cloud.png')}}" alt="cloud-img">
+        <h1 class="head">Merry Christmas</h1>
+      <h2><p>"Lets Celebrate!!!!!!!!"</p></h2>
+        <img class="bottom-cloud"src="{{url('media\cloud.png')}}" alt="cloud-img">
+        <img class="images" src="{{url('media\santa_claus_PNG9988.png')}}" alt="mountain-img">
+  </div>
 
-     <h2>altered</h2>
-
+    <div class="skills">
+      <h3>Christmas Quotes</h3>
+      <div class="skill-row">
+        <img class="skill-1" src="{{url('media/chillies.png')}}" alt="">
+        <h3>Nora Roberts</h3>
+        <p>"Nothing ever seems too bad, too hard, or too sad when you've got a Christmas tree in the living room
+        </p>
+    </div>
+    <div class="skill-row">
+    <h3>Ruth Carter Stapleton</h3>
+    <img class="fish" src="{{url('media/cloud.png')}}" alt="">
+    <p>"Christmas is most truly Christmas when we celebrate it by giving the light of love to those who need it most."</p>
+    </div>
+    </div>
+    <hr>
+    <div class="contact-me">
+    <h2>Get In Touch</h2>
+    <h3><p class="contacti">Want to Know more ?👇</p></h3>
+    <a class="btn" href="mailto:naseefmottath@gmail.com">CONTACT ME</a>
+    </div>
+    </div>
+    <div class="bottom-container">
+    <a class="footer-link" href="https://www.linkedin.com/">LinkedIn</a>
+    <a class="footer-link" href="https://twitter.com/">Twitter</a>
+    <a class="footer-link" href="https://www.google.com/">Website</a>
+    <p class="copyright">© 2021 Mohammed Naseef@ mottath</p>
+    </div>
 
 </body>
 </html>
